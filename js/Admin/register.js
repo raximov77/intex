a0b7f30bfd1110bddc3c5d5ca6379c08b47c61e6
raximov77 <raximovikrom68@gmail.com>
2024-08-28 08:01:39 +0500

    Update

---
+++ js/Admin/register.js
@@ -0,0 +1,16 @@
+
+
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    
+    if(username === "admin" && password === "123") {
+        location.pathname = "../../admin.html"
+        alert("Login successful!");
+    } else {
+        alert("Invalid username or password!");
+    }
+});
